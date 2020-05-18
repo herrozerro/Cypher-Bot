6fbf5dcf0aff7eed2b9861867abe8229107e8b95
Josiah Bradbury <herrozerro@gmail.com>
2020-05-18 13:08:02 -0500

    testing data

--- src/CypherBot.Web/Controllers/HomeController.cs
+++ src/CypherBot.Web/Controllers/HomeController.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CypherBot.Web.Models;
+using CypherBot.Core.DataAccess.Repos;
 
 namespace CypherBot.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CypherContext _cypherContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CypherContext cypherContext)
         {
             _logger = logger;
+            _cypherContext = cypherContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_cypherContext.Cyphers.Take(10).ToList());
         }
 
         public IActionResult Privacy()
