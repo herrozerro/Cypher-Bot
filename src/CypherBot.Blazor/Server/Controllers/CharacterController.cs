c2435f0928213a1c9b6384fe51beff16cd7d1333
Josiah Bradbury <herrozerro@gmail.com>
2020-05-26 23:12:54 -0500

    Added authorize on endpoints and views

--- src/CypherBot.Blazor/Server/Controllers/CharacterController.cs
+++ src/CypherBot.Blazor/Server/Controllers/CharacterController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using CypherBot.Core.DataAccess.Repos;
 using CypherBot.Core.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CypherBot.Blazor.Server.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CharacterController : ControllerBase
